7fe116bc05487946083891b0ba2d6c96686c853c
KasraRahimi <kasra.rahimi@polymtl.ca>
2022-11-30 20:55:58 -0500

    NEW added addRoom() method

--- lib_dir/include/Room.h
+++ lib_dir/include/Room.h
@@ -2,12 +2,20 @@
 #include <iostream>
 #include <memory>
 
+enum class Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+};
+
 class Room {
 public:
     Room();
     Room(std::string name, std::string description, bool isLocked=false);
+    void addRoom(std::shared_ptr<Room> room, Direction side);
 private:
     std::string name_, description_;
     std::shared_ptr<Room> northRoom_, eastRoom_, southRoom_, westRoom_;
-    bool isLocked;
+    bool isLocked_;
 };
