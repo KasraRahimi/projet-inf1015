b0c4b6072659bc4240819534901cadfe863249f0
KasraRahimi <kasra.rahimi@polymtl.ca>
2022-12-01 01:40:20 -0500

    NEW added several methods

--- lib_dir/include/Game.h
+++ lib_dir/include/Game.h
@@ -1,14 +1,21 @@
 #pragma once
 #include "Room.h"
+#include <vector>
 
 class Game {
 public:
     Game();
     Game(Room &defaultRoom);
     std::string getUserInput();
+    void processCommande(std::string command);
+
+    bool isRunning_() const;
 private:
     Room* currentRoom_;
+    bool isRunning_;
 
     void changeRoom();
     void printCurrentRoom();
+    void printNeighboringRoom(const Room* ptrRoom, Direction side);
+    std::vector<std::string> stringToVectorOfWords(std::string sentence);
 };
