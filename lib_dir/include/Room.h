cb8e3f0ed7d3e94d1078396545ef0aca618cdb1f
KasraRahimi <kasra.rahimi@polymtl.ca>
2022-11-30 22:15:40 -0500

    CHANGE added const on methods that dont change class

--- lib_dir/include/Room.h
+++ lib_dir/include/Room.h
@@ -14,10 +14,10 @@ public:
     Room();
     Room(std::string name, std::string description, bool isLocked=false);
     void addRoom(Room &room, Direction side);
-    std::string getName();
-    std::string getDescription();
-    Room* getAdjacentRoomPtr(Direction side);
-    bool isLocked();
+    std::string getName() const;
+    std::string getDescription() const;
+    Room* getAdjacentRoomPtr(Direction side) const;
+    bool isLocked() const;
 private:
     std::string name_, description_;
     Room* northRoom_;
