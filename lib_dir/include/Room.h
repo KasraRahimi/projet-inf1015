d6777b626596b7712203e1d71e0d1f33e217d869
KasraRahimi <kasra.rahimi@polymtl.ca>
2022-11-30 21:39:55 -0500

    NEW added several getters in the Room class

--- lib_dir/include/Room.h
+++ lib_dir/include/Room.h
@@ -14,6 +14,10 @@ public:
     Room();
     Room(std::string name, std::string description, bool isLocked=false);
     void addRoom(std::shared_ptr<Room> room, Direction side);
+    std::string getName();
+    std::string getDescription();
+    std::shared_ptr<Room> getAdjacentRoom(Direction side);
+    bool isLocked();
 private:
     std::string name_, description_;
     std::shared_ptr<Room> northRoom_, eastRoom_, southRoom_, westRoom_;
