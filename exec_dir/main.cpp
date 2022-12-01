8ed19f06083dfc11d92cd7feea8d243f735d47b5
KasraRahimi <kasra.rahimi@polymtl.ca>
2022-12-01 12:09:14 -0500

    NEW small functional game

--- exec_dir/main.cpp
+++ exec_dir/main.cpp
@@ -1,3 +1,15 @@
+#include <Game.h>
+
 int main() {
+    Room LivingRoom("Living Room",
+        "The living room is a spacious room. It has a TV and a couch.");
+    Room Kitchen("Kitchen",
+        "The kitchen has a fridge, an oven, and many pots and pans!");
+    LivingRoom.addRoom(Kitchen, Direction::NORTH);
+    Game game(LivingRoom);
+    while (game.isRunning()) {
+        std::string command = game.getUserInput();
+        game.processCommande(command);
+    }
     return 0;
 }
