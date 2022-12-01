e453b971f7c7060aca99356457b37e5d2ab9548c
KasraRahimi <kasra.rahimi@polymtl.ca>
2022-12-01 12:23:17 -0500

    NEW added three more rooms. Livrable 1 is mostly done.

--- exec_dir/main.cpp
+++ exec_dir/main.cpp
@@ -1,12 +1,23 @@
 #include <Game.h>
 
 int main() {
-    Room LivingRoom("Living Room",
+    Room livingRoom("Living Room",
         "The living room is a spacious room. It has a TV and a couch.");
-    Room Kitchen("Kitchen",
+    Room kitchen("Kitchen",
         "The kitchen has a fridge, an oven, and many pots and pans!");
-    LivingRoom.addRoom(Kitchen, Direction::NORTH);
-    Game game(LivingRoom);
+    Room hallway("Hallway",
+        "The hallway connects the living room to the bathroom. It's kinda dark. The light went out.");
+    Room bathroom("Bathroom",
+        "The bathroom has a toilet and a bathtub. It's rather small.");
+    Room bedroom("Bedroom",
+        "The bedroom is large. It has a bed and a desk. Your closet can be found here.");
+
+    
+    livingRoom.addRoom(kitchen, Direction::NORTH);
+    livingRoom.addRoom(hallway, Direction::EAST);
+    hallway.addRoom(bathroom, Direction::SOUTH);
+    hallway.addRoom(bedroom, Direction::NORTH);
+    Game game(livingRoom);
     while (game.isRunning()) {
         std::string command = game.getUserInput();
         game.processCommande(command);
