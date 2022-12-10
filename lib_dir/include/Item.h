668d9179f5ffcba85548ff5b8313c592227f46f6
KasraRahimi <kasra.rahimi@polymtl.ca>
2022-12-10 17:43:32 -0500

    NEW added several functions to Item class

--- lib_dir/include/Item.h
+++ lib_dir/include/Item.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <iostream>
+#include <vector>
+using vecOfStrings = std::vector<std::string>;
+
+class Item {
+public:
+    Item() = default;
+    virtual ~Item() = default;
+    std::string getName() { return name_; }
+    std::string getDescription() { return description_; }
+    bool isInKeyWords(std::string word);
+private:
+    std::string name_, description_;
+    vecOfStrings keyWords;
+    bool isTakeable_;
+};
