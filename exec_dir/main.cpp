e4bb533c44f7aac68d1af0e97e93f657bfb71977
KasraRahimi <kasra.rahimi@polymtl.ca>
2022-12-18 11:34:37 -0500

    CHANGE room setup in main is now semi-final

--- exec_dir/main.cpp
+++ exec_dir/main.cpp
@@ -7,40 +7,69 @@
 #include "Inventory.h"
 
 int main() {
-    Room livingRoom("Living Room",
-        "The living room is a spacious room. It has a TV and a couch.");
-    Room kitchen("Kitchen",
-        "The kitchen has a fridge, an oven, and many pots and pans!");
-    Room hallway("Hallway",
-        "The hallway connects the living room to the bathroom. It's kinda dark. The light went out.",
+    Room foyer("foyer",
+        "This is the entrance to the house. Not much to see here.");
+    Room livingRoom("living room",
+        "This room is so cozy. There's a couch and TV!");
+    Room kitchen("kitchen", 
+        "Pots, pans, and dishes everywhere! Someone should really clean up here.");
+    Room hallway1("hallway1",
+        "This is the section of the hallway connected to the living room.");
+    Room hallway2("hallway2",
+        "This section of the hallway is connected to the bedroom.");
+    Room bedroom("bedroom",
+        "A nice bed, a good amount of storage. Hmm this seems like a nice bedroom");
+    Room pianoRoom("piano room",
+        "This room is a mess. Sheet music everywhere, spider webs in every corner. How could someone let it get this bad?",
         true);
-    Room bathroom("Bathroom",
-        "The bathroom has a toilet and a bathtub. It's rather small.");
-    Room bedroom("Bedroom",
-        "The bedroom is large. It has a bed and a desk. Your closet can be found here.",
+    Room closet("closet",
+        "This appears to be a regular closet. What can I find in here?");
+    Room narnia("narnia",
+        "Holy cow, this place is magical! Why do I have to pass through a closet to get here?",
         true);
-    Room foyer("Foyer",
-        "The foyer is at the entrance of the house. There are many more rooms to explore!");
+    Room study("study",
+        "This room is a little messy, nothing too bad. It's just a little too dark.",
+        true);
+    Room bathroom("bathroom",
+        "This is the fanciest bathroom I've ever seen in my life. \
+        The toilet is made of gold! The toilet paper is made of $100 bills!");
 
-    foyer.addRoom(livingRoom, Direction::NORTH);
+    livingRoom.addRoom(foyer, Direction::SOUTH);
     livingRoom.addRoom(kitchen, Direction::NORTH);
-    livingRoom.addRoom(hallway, Direction::EAST);
-    hallway.addRoom(bathroom, Direction::SOUTH);
-    hallway.addRoom(bedroom, Direction::NORTH);
+    livingRoom.addRoom(hallway1, Direction::EAST);
+
+    hallway1.addRoom(pianoRoom, Direction::SOUTH);
+    hallway1.addRoom(hallway2, Direction::EAST);
+
+    closet.addRoom(pianoRoom, Direction::NORTH);
+    closet.addRoom(narnia, Direction::SOUTH);
+
+    hallway2.addRoom(study, Direction::NORTH);
+    hallway2.addRoom(bedroom, Direction::EAST);
+    hallway2.addRoom(bathroom, Direction::SOUTH);
 
     const std::string name = "INF1015 KASRA & IMANE'S ROOM ADVENTURE";
     Game game(foyer, name);
 
-    kitchen.getInventory().addItem(std::make_unique<Key>("small sticky blue key",
-        "This key is blue and sticky. It's uncomfortable to touch. It seems rather small.",
-        livingRoom,
-        hallway,
-        game));
-    kitchen.getInventory().addItem(std::make_unique<Key>("small dry golden key",
-        "This key is small, but it's golden and majestic. What could it be for?",
-        hallway,
-        bedroom,
-        game));
+    auto pianoRoomKeyPtr = std::make_unique<Key>("large silver key",
+        "This key seems large and important. It's a little rusty.",
+        hallway1,
+        pianoRoom,
+        game);
+    auto narniaKeyPtr = std::make_unique<Key>("tiny rainbow colored key",
+        "Really weird key. It doesn't look normal at all. What could it be for?",
+        closet,
+        narnia,
+        game);
+    auto studyKeyPtr = std::make_unique<Key>("mid-sized golden key",
+        "This key seems majestic. Golden colored and everything!",
+        hallway2,
+        study,
+        game);
+
+    kitchen.getInventory().addItem(std::move(pianoRoomKeyPtr));
+    closet.getInventory().addItem(std::move(narniaKeyPtr));
+    narnia.getInventory().addItem(std::move(studyKeyPtr));
 
     while (game.isRunning()) {
         std::string command = game.getUserInput();
