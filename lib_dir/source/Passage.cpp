c8a9f513fe930819eec79001accb82f6dc0c3210
KasraRahimi <kasra.rahimi@polymtl.ca>
2022-12-18 23:02:30 -0500

    CHANGE renamed boolean

--- lib_dir/source/Passage.cpp
+++ lib_dir/source/Passage.cpp
@@ -1,11 +1,11 @@
 #include "Passage.h"
 #include "Inventory.h"
 
-Passage::Passage(std::string name, std::string desc, Room& source, Room& destination, Game& game, bool isAddingToDestination) :
+Passage::Passage(std::string name, std::string desc, Room& source, Room& destination, Game& game, bool isTwoWay) :
     Item(name, desc, false),
     destination_(&destination),
     game_(&game) {
-        if (isAddingToDestination) {
+        if (isTwoWay) {
             auto stairPtr = std::make_unique<Passage>(name, desc, destination, source, game, false);
             destination.getInventory().addItem(std::move(stairPtr));
         }
